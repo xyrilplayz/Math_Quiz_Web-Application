2ca1c98b46ae55c63477753946808c2c51c17b3f
xyrilplayz <152459199+xyrilplayz@users.noreply.github.com>
2024-12-02 22:37:36 +0800

    results

---
+++ results.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz Results</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<div class="container-fluid p-5 bg-primary text-white text-center">
+    <h1>Quiz Results</h1>
+    <p>Name: <?php echo $_SESSION['name']; ?></p>
+    <p>Total Number of Questions: <?php echo $_SESSION['questions']; ?></p>
+    <p>Your Correct Answer/s: <?php echo $_SESSION['correct']; ?></p>
+    <p>Your Wrong Answer/s: <?php echo $_SESSION['wrong']; ?></p>
+</div>
+    <a href="index.php">Try Again</a>
+
+</body>
+</html>
+
+<?php
+session_destroy();
+?>
