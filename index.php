a2bc4b9bd6e8ba048791e4fc1d4ae1048e4b6f46
xyrilplayz <152459199+xyrilplayz@users.noreply.github.com>
2024-12-02 22:04:17 +0800

    starting

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<div class="container-fluid p-5 bg-primary text-white text-center">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Math Quiz Login</title>
+</head>
+<body>
+    <h1>Welcome to my Math Quiz Application</h1>
+    <form action="quiz.php" method="POST">
+        <label for="name">Enter your name:</label>
+        <input type="text" name="name" required><br><br>
+        
+        <label for="level">Select Level:</label>
+</body>
+</html>
