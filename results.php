739f0fd3a2f475c10ed039290113b09637bc364b
xyrilplayz <152459199+xyrilplayz@users.noreply.github.com>
2024-12-02 22:38:41 +0800

    with css becuase we cant see the try again

--- results.php
+++ results.php
@@ -11,6 +11,12 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+    <style>
+        #color {
+            background: black;
+            max-width: 80px;
+        }
+    </style>
 <body>
 <div class="container-fluid p-5 bg-primary text-white text-center">
     <h1>Quiz Results</h1>
@@ -19,8 +25,9 @@ session_start();
     <p>Your Correct Answer/s: <?php echo $_SESSION['correct']; ?></p>
     <p>Your Wrong Answer/s: <?php echo $_SESSION['wrong']; ?></p>
 </div>
+<div id="color">
     <a href="index.php">Try Again</a>
-
+</div>
 </body>
 </html>
 
