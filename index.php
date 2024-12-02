4f7b24840f905d6a29ee06a4f1c7655c738581e1
xyrilplayz <152459199+xyrilplayz@users.noreply.github.com>
2024-12-02 22:15:19 +0800

    with bootstrap from www3school

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <div class="container-fluid p-5 bg-primary text-white text-center">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Math Quiz Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
+    <div class="container-fluid p-5 bg-primary text-white text-center">
     <h1>Welcome to my Math Quiz Application</h1>
+    <div class="container col-sm-2">
     <form action="quiz.php" method="POST">
         <label for="name">Enter your name:</label>
         <input type="text" name="name" required><br><br>
@@ -25,7 +28,7 @@
 
         <button type="submit">Start Quiz</button>
     </form>
-
+    </div>
 </body>
 <script>
     const levelSelector = document.querySelector('select[name="level"]');
